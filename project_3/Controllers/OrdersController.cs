1f725e81caccb3a6f59e65fd7e9f209b382c1408
unknown <litterfail97@gmail.com>
2020-07-03 00:21:43 +0700

    update code

---
+++ project_3/Controllers/OrdersController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using project_3.Models;
+
+namespace project_3.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private ShowRoomManagerEntities db = new ShowRoomManagerEntities();
+
+        // GET: Orders
+        public ActionResult Index()
+        {
+            var orders = db.Orders.Include(o => o.customer).Include(o => o.Employee).Include(o => o.Product);
+            return View(orders.ToList());
+        }
+
+        // GET: Orders/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // GET: Orders/Create
+        public ActionResult Create()
+        {
+            ViewBag.id_customer = new SelectList(db.customers, "id_customer", "name");
+            ViewBag.id_employee = new SelectList(db.Employees, "id_employee", "name");
+            ViewBag.id_product = new SelectList(db.Products, "id_product", "name");
+            return View();
+        }
+
+        // POST: Orders/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id_order,id_product,id_employee,id_customer,title,time,vat")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Orders.Add(order);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.id_customer = new SelectList(db.customers, "id_customer", "name", order.id_customer);
+            ViewBag.id_employee = new SelectList(db.Employees, "id_employee", "name", order.id_employee);
+            ViewBag.id_product = new SelectList(db.Products, "id_product", "name", order.id_product);
+            return View(order);
+        }
+
+        // GET: Orders/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.id_customer = new SelectList(db.customers, "id_customer", "name", order.id_customer);
+            ViewBag.id_employee = new SelectList(db.Employees, "id_employee", "name", order.id_employee);
+            ViewBag.id_product = new SelectList(db.Products, "id_product", "name", order.id_product);
+            return View(order);
+        }
+
+        // POST: Orders/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id_order,id_product,id_employee,id_customer,title,time,vat")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(order).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.id_customer = new SelectList(db.customers, "id_customer", "name", order.id_customer);
+            ViewBag.id_employee = new SelectList(db.Employees, "id_employee", "name", order.id_employee);
+            ViewBag.id_product = new SelectList(db.Products, "id_product", "name", order.id_product);
+            return View(order);
+        }
+
+        // GET: Orders/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // POST: Orders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Order order = db.Orders.Find(id);
+            db.Orders.Remove(order);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ project_3/Controllers/showroomsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using project_3.Models;
+
+namespace project_3.Controllers
+{
+    public class showroomsController : Controller
+    {
+        private ShowRoomManagerEntities db = new ShowRoomManagerEntities();
+
+        // GET: showrooms
+        public ActionResult Index()
+        {
+            var showrooms = db.showrooms.Include(s => s.Product);
+            return View(showrooms.ToList());
+        }
+
+        // GET: showrooms/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            showroom showroom = db.showrooms.Find(id);
+            if (showroom == null)
+            {
+                return HttpNotFound();
+            }
+            return View(showroom);
+        }
+
+        // GET: showrooms/Create
+        public ActionResult Create()
+        {
+            ViewBag.id_product = new SelectList(db.Products, "id_product", "name");
+            return View();
+        }
+
+        // POST: showrooms/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id_showroom,name,address,phonenumber,email,id_product,id_company")] showroom showroom)
+        {
+            if (ModelState.IsValid)
+            {
+                db.showrooms.Add(showroom);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.id_product = new SelectList(db.Products, "id_product", "name", showroom.id_product);
+            return View(showroom);
+        }
+
+        // GET: showrooms/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            showroom showroom = db.showrooms.Find(id);
+            if (showroom == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.id_product = new SelectList(db.Products, "id_product", "name", showroom.id_product);
+            return View(showroom);
+        }
+
+        // POST: showrooms/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id_showroom,name,address,phonenumber,email,id_product,id_company")] showroom showroom)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(showroom).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.id_product = new SelectList(db.Products, "id_product", "name", showroom.id_product);
+            return View(showroom);
+        }
+
+        // GET: showrooms/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            showroom showroom = db.showrooms.Find(id);
+            if (showroom == null)
+            {
+                return HttpNotFound();
+            }
+            return View(showroom);
+        }
+
+        // POST: showrooms/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            showroom showroom = db.showrooms.Find(id);
+            db.showrooms.Remove(showroom);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
